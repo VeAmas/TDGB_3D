4139ed7115506db28bbd5e43b031a0172ce38f97
chenht <chenht@kongtrolink.com>
2019-12-20 17:35:32 +0800

    [change]

---
+++ Assets/Script/FollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform playerTransform;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = playerTransform.position + offset;
+        
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 200;
+    public float sideForce = 200;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        if (Input.GetKey("d")) {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        } else if (Input.GetKey("a")) {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
