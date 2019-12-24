61ab85d56de044f044f26c840e1a89b156228887
chenht <chenht@kongtrolink.com>
2019-12-24 18:07:29 +0800

    [change]

---
+++ Assets/Communication.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.InteropServices;
+
+public class Communication : MonoBehaviour {
+    //引用jslib的Hello()方法
+    [DllImport("__Internal")]
+    private static extern void Hello();
+    //测试UI，我们的目的是用jslib的Hello()方法调用网页的js方法。然后用js方法调用Unity方法。
+    //这样就完成了Unity和网页中的方法互相调用。最后MyText会显示Hello, world!
+    public Text MyText;
+
+    void Start() {
+        //我们程序一开始就调用jslib的方法Hello()
+        Hello();
+    }
+    //测试 网页调用此方法
+    public void TestMethod(string text) {
+        MyText.text = text;
+    }
+}
---
+++ Assets/ObjectSelect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSelect : MonoBehaviour
+{
+
+    bool isMouseDown = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !isMouseDown) {
+            isMouseDown = true;
+            Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.farClipPlane));
+            Ray ray = new Ray(Camera.main.transform.position, mouseWorldPosition - Camera.main.transform.position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                Debug.Log(hit.collider.gameObject.name);
+            }
+        } else if (Input.GetMouseButtonUp(0)) {
+            isMouseDown = false;
+        }
+
+    }
+}
